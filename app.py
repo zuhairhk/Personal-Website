44294e75bfc608f7e554dc2930084237c9da31a0
zuhairhk <zuhairhk@gmail.com>
2022-06-27 15:12:46 -0400

    Big change update

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from flask import render_template, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>Home Page</h1>"
+def index():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/sotw')
+def sotw():
+    return render_template('sotw.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
