5860f3103b396b176f1f277996345ad1c67f8d45
zuhairhk <zuhairhk@gmail.com>
2022-08-09 12:18:52 -0400

    song update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('paging.html')
-    # CHANGE WHEN COMPLETE --> return render_template('index.html')
+    # return render_template('index.html')
 
 @app.route('/blog')
 def blog():
